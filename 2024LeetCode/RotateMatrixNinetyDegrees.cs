24010a3cc1b338ac936541a1f667ef2954921680
aviman1258 <achandra1258@gmail.com>
2024-03-29 17:03:11 -0700

    Optimized RotateMatrixNinetyDegrees

--- 2024LeetCode/RotateMatrixNinetyDegrees.cs
+++ 2024LeetCode/RotateMatrixNinetyDegrees.cs
@@ -19,33 +19,115 @@
             //  9 10 11 12       15 11  7  3
             // 13 14 15 16       16 12  8  4
         }
-        
+
         private static void TestRotate(int[][] input, int[][] expected)
         {
-            int[][] actual = Rotate(input);
-            if (Enumerable.SequenceEqual(input.SelectMany(inner => inner), expected.SelectMany(inner => inner)))
+            int[][] actual = RotateInPlace(input);
+
+            bool pass = true;
+
+            for (int q = 0; q < input.Length; q++)
+            {
+                for (int w = 0; w < input[q].Length; w++)
+                {
+                    if (actual[q][w] != expected[q][w])
+                        pass = false;
+                }
+            }
+
+            if (pass)
             {
                 Console.WriteLine("Rotate Pass!");
-                return;
             }
-            Console.WriteLine("Rotate Fail!");
+            else
+            {
+                Console.WriteLine("Rotate Fail!");
+            }
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                // Label for expected array
+                Console.Write("expected: ");
+                for (int j = 0; j < expected[i].Length; j++)
+                {
+                    Console.Write(expected[i][j].ToString().PadLeft(4) + " ");
+                }
+                Console.Write(" : ");
+
+                // Label for input array
+                Console.Write("input: ");
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    Console.Write(input[i][j].ToString().PadLeft(4) + " ");
+                }
+                Console.Write(" --> ");
+
+                // Label for result array
+                Console.Write("actual: ");
+                for (int j = 0; j < actual[i].Length; j++)
+                {
+                    Console.Write(actual[i][j].ToString().PadLeft(4) + " ");
+                }
+                Console.WriteLine(); // Move to the next line after each row
+            }
         }
 
+        /// <summary>
+        /// Rotate n x n matrix 90 degrees clockwise. Time complexity O(n^2). Space compexity O(n^2)
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
         private static int[][] Rotate(int[][] matrix)
         {
             int[][] rotatedMatrix = new int[matrix.Length][];
             int n = matrix.Length;
 
+            for (int a = 0; a < n; a++)
+            {
+                rotatedMatrix[a] = new int[n];
+            }
+
             for (int i = 0; i < n; i++)
             {
-                for(int j = 0; j < n; j++)
+                for (int j = 0; j < n; j++)
                 {
                     rotatedMatrix[j][(n - 1) - i] = matrix[i][j];
                 }
             }
 
             return rotatedMatrix;
-            //test change
+        }
+
+        /// <summary>
+        /// Rotate n x n matrix 90 degrees clockwise. Time complexity O(n^4). Space compexity O(n)
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        private static int[][] RotateInPlace(int[][] matrix)
+        {
+            int n = matrix.Length;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = i + 1; j < n; j++)
+                {
+                    matrix[i][j] = matrix[i][j] + matrix[j][i];
+                    matrix[j][i] = matrix[i][j] - matrix[j][i];
+                    matrix[i][j] = matrix[i][j] - matrix[j][i];
+                }
+            }
+
+            for (int a = 0; a < n; a++)
+            {
+                for(int b = 0; b < n/2; b++)
+                {
+                    matrix[a][b] = matrix[a][b] + matrix[a][n-b-1];
+                    matrix[a][n-b-1] = matrix[a][b] - matrix[a][n-b-1];
+                    matrix[a][b] = matrix[a][b] - matrix[a][n-b-1];
+                }
+            }
+
+            return matrix;
         }
     }
 }
