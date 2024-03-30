571e2bc76d72ad478af7712fc7de6878e2d348a3
aviman1258 <achandra1258@gmail.com>
2024-03-30 11:21:32 -0700

    Finished AddTwoNumbers

--- 2024LeetCode/AddTwoNumbers.cs
+++ 2024LeetCode/AddTwoNumbers.cs
@@ -14,7 +14,11 @@
     {
         public static void TestAdd2Numbers()
         {
-            
+            TestAdd2Numbers(IntToListNode(123), IntToListNode(256), IntToListNode(379));
+            TestAdd2Numbers(IntToListNode(23), IntToListNode(176), IntToListNode(199));
+            TestAdd2Numbers(IntToListNode(0), IntToListNode(0), IntToListNode(0));
+            TestAdd2Numbers(IntToListNode(100), IntToListNode(0), IntToListNode(100));
+            TestAdd2Numbers(IntToListNode(2), IntToListNode(32234), IntToListNode(32236));
         }
         
         private static void TestAdd2Numbers(ListNode input1, ListNode input2, ListNode expected)
@@ -70,9 +74,49 @@
             Console.WriteLine(string.Format("Add2Numbers Fail! {0} + {1}; Expected: {2}; Actual: {3};", input1Int, input2Int, expectedInt, actualInt));
         }
 
-        public static ListNode Add2Numbers(ListNode l1, ListNode l2)
+        private static ListNode Add2Numbers(ListNode l1, ListNode l2)
         {
-            return new ListNode();
+            ListNode currentL1 = l1;
+            int currentL1TensPlace = 1;
+            int l1Int = 0;
+
+            while(currentL1 != null)
+            {
+                l1Int += currentL1.val * currentL1TensPlace;
+                currentL1TensPlace *= 10;
+                currentL1 = currentL1.next;
+            }
+
+            ListNode currentL2 = l2;
+            int currentL2TensPlace = 1;
+            int l2Int = 0;
+
+            while (currentL2 != null)
+            {
+                l2Int += currentL2.val * currentL2TensPlace;
+                currentL2TensPlace *= 10;
+                currentL2 = currentL2.next;
+            }
+
+            int result = l1Int + l2Int;
+
+            return IntToListNode(result);
+        }
+
+        private static ListNode IntToListNode(int num)
+        {
+            ListNode head = new();
+            ListNode current = head;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                num /= 10;
+
+                current.next = new ListNode(digit);
+                current = current.next;
+            }
+
+            return head.next;
         }
     }
 }
