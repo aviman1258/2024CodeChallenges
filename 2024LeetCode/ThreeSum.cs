ee530d24ca2de141fbb847bd566efcdf1bc7bf00
aviman1258 <achandra1258@gmail.com>
2024-04-07 17:13:30 -0700

    Refactor

---
+++ 2024LeetCode/ThreeSum.cs
@@ -0,0 +1,25 @@
+﻿using static _2024LeetCode.Test.Helpers;
+
+namespace _2024LeetCode
+{
+    internal class ThreeSum
+    {
+        private static void TestGetThreeSums(int[] input, IList<IList<int>> expected)
+        {
+            IList<IList<int>> actual = GetThreeSums(input);
+            if (AreListEqual((List<List<int>>) actual, (List<List<int>>) expected))
+            {
+                Console.WriteLine(string.Format("Three Sum Pass! Array to analyze: {0}; Expected: {1}; Actual: {2};", input, expected, actual));
+                return;
+            }
+            Console.WriteLine(string.Format("LongestPalindrome Pass! String to analyze: {0}; Expected: {1}; Actual: {2};", input, expected, actual));
+        }
+
+        
+
+        private static IList<IList<int>> GetThreeSums(int[] nums)
+        {
+
+        }
+    }
+}
